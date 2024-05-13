39def6678724d8176a45bbea7f8c60b075eb4cfb
Vladyslav-Holovin <golovinvlad2001@gmail.com>
2024-05-13 13:10:23 +0300

    last update

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,21 @@
-function filterArray(numbers, value) {
-  let finalArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      finalArray.push(numbers[i]);
-    }
-  }
-  return finalArray;
-}
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  getInfo() {
+    return `${profile.username} has ${profile.playTime} active hours!`;
+  },
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
